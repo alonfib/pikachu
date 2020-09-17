5fd7e8eaf98f81d96cabd14b0ca0ee52ced38bc1
Alon Fibert <alonfib@gmail.com>
2020-09-17 14:47:40 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Pikachu from './Pikachu';
+
+ReactDOM.render(
+    <Pikachu />,
+  document.getElementById('root')
+);
