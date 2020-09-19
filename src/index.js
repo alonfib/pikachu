f29e2bc2c8bad6016db8e7570c29f608102416ff
Alon Fibert <alonfib@gmail.com>
2020-09-20 02:03:51 +0300

    overflow hidden

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Pikachu from "./Pikachu/Pikachu";
 
-ReactDOM.render(<Pikachu />, document.getElementById("root"));
+const jsx = (
+  <div className="container">
+    <Pikachu />
+  </div>
+);
+ReactDOM.render(jsx, document.getElementById("root"));
